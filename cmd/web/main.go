80e997fdf2881d08d657f34aa93634f50475ce14
Ayrat Musin <musinayrat@gmail.com>
2023-11-21 19:00:43 +0300

    added DI app, db connection, refactoring

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,33 +1,54 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"log"
 	"net/http"
 	"os"
+
+	_ "github.com/jackc/pgx/v5/stdlib"
 )
+
+type application struct {
+	errorLog *log.Logger
+	infoLog  *log.Logger
+}
 
 func main() {
 	addr := flag.String("addr", ":4000", "HTTP network address")
+	dsn := flag.String("dsn", "postgresql://localhost/snippetbox?user=web@localhost&password=pass", "Postgres data source name")
 	flag.Parse()
 
-	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
-	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+	app := &application{
+		errorLog: log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile),
+		infoLog:  log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime),
+	}
 
-	mux := http.NewServeMux()
-	fileServer := http.FileServer(http.Dir("./ui/static"))
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
-	mux.HandleFunc("/", home)
-	mux.HandleFunc("/snippet/view", snippetView)
-	mux.HandleFunc("/snippet/create", snippetCreate)
+	db, err := openDB(*dsn)
+	if err != nil {
+		app.errorLog.Fatal(err)
+	}
+	defer db.Close()
 
 	srv := &http.Server{
 		Addr:     *addr,
-		ErrorLog: errorLog,
-		Handler:  mux,
+		ErrorLog: app.errorLog,
+		Handler:  app.routes(),
 	}
 
-	infoLog.Printf("Starting server on %s", *addr)
-	err := srv.ListenAndServe()
-	errorLog.Fatal(err)
+	app.infoLog.Printf("Starting server on %s", *addr)
+	err = srv.ListenAndServe()
+	app.errorLog.Fatal(err)
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("pgx", dsn)
+	if err != nil {
+		return nil, err
+	}
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+	return db, nil
 }
